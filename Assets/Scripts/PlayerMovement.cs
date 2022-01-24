5b9fe93b2122deaa91a552f4cecab87b7a7a6229
Zeke <zekeabshire@gmail.com>
2022-01-24 02:07:55 -0600

    upgraded to new input system

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ClawBrawl
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed;
+
+        private Vector2 inputDir;
+
+        private void Update()
+        {
+            HandleMove();
+            HandleRotate();
+        }
+
+        public void MovePlayer(InputAction.CallbackContext context)
+        {
+            if (context.started) return;
+
+            inputDir = context.ReadValue<Vector2>();
+
+        }
+
+        private void HandleMove()
+        {
+            Vector3 moveDir = Vector3.forward * inputDir.y + Vector3.right * inputDir.x;
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
+        private void HandleRotate()
+        {
+            if (inputDir == Vector2.zero) return;
+
+            Vector3 lookDir = new Vector3(inputDir.x, 0, inputDir.y);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDir), 0.7f);
+        }
+    }
+}
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -6,32 +6,6 @@ namespace ClawBrawl
 {
     public class WeaponController : MonoBehaviour
     {
-        [SerializeField] private GameObject weaponModel;
-        [SerializeField] private Vector3 originalPos;
-        [SerializeField] private bool canAttack = true;
-        [SerializeField] private bool isAttacking = false;
-        [SerializeField] private bool weaponIsLeaving = false;
-        [SerializeField] private float throwRange = 10;
 
-        private void Awake()
-        {
-            originalPos = transform.position;
-        }
-
-        private void Update()
-        {
-            if (isAttacking)
-            {
-                if (Vector3.Distance(weaponModel.transform.position, originalPos) > throwRange)
-                    weaponModel.transform.position += weaponModel.transform.forward * 1;
-                isAttacking = false;
-            }
-        }
-
-        public void HandleAttack()
-        {
-            Debug.Log("Attacking");
-            isAttacking = true;
-        }
     }
 }
